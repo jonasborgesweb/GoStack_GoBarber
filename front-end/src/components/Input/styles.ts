8b9b70399e1e5d8912a5a50e10324b716dc55460
jonasborgesweb <jonasborgesweb@gmail.com>
2020-04-22 15:50:47 -0300

    Feito a validação dos dados SingUp

--- front-end/src/components/Input/styles.ts
+++ front-end/src/components/Input/styles.ts
@@ -1,13 +1,20 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   font-size: 18px;
   font-weight: 400;
   background-color: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
-  padding: 18px 16px;
+  padding: 0 16px;
   width: 100%;
+  transition: 0.3s ease;
+
+  border: 2px solid #232129;
   color: #666360;
 
   display: flex;
@@ -17,6 +24,20 @@ export const Container = styled.div`
     margin-bottom: 8px;
   }
 
+  ${(props) =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+      color: #ff9000;
+      box-shadow: 0 3px 9px rgba(0, 0, 0, 0.3);
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
   input {
     flex: 1;
     border: 0;
@@ -25,6 +46,7 @@ export const Container = styled.div`
     box-sizing: border-box;
     color: #f4ede8;
     transition: 0.2s ease;
+    padding: 18px 0;
 
     &::placeholder {
       color: #666360;
