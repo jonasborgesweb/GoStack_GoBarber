2e3b784ae56737b7c51bf6caa57e89ac20126d15
jonasborgesweb <jonasborgesweb@gmail.com>
2020-04-22 16:40:54 -0300

    Adicionado o Cors

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,36 +1,40 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction, response } from 'express';
-import 'express-async-errors'
+import cors from 'cors';
+import 'express-async-errors';
 
 import routes from './routes';
-import uploadConfig from './config/upload'
+import uploadConfig from './config/upload';
 
-import './database'
+import './database';
 import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory))
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.log(err);
+
+    return response.status(500).json({
       status: 'error',
-      message: err.message
-    })
-  }
-
-  console.log(err);
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error'
-  })
-});
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('🚀️ Server started on port 3333');
